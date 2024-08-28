692bde7a8942ce68996fc374e0595b6888f10445
Farzin <farzinallahyari1234@gmail.com>
2024-08-28 11:04:51 +0330

    set up local db

--- tracker/tracker_data/src/main/java/com/farzin/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/farzin/tracker_data/di/TrackerDataModule.kt
@@ -1,11 +1,14 @@
 package com.farzin.tracker_data.di
 
+import android.content.Context
+import androidx.room.Room
+import com.farzin.tracker_data.local.TrackerDatabase
 import com.farzin.tracker_data.remote.OpenFoodApi
 import com.farzin.tracker_data.remote.OpenFoodApi.Companion.BASE_URL
-import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -32,7 +35,7 @@ object TrackerDataModule {
 
     @Provides
     @Singleton
-    fun provideOpenFoodApi(okHttpClient: OkHttpClient):OpenFoodApi =
+    fun provideOpenFoodApi(okHttpClient: OkHttpClient): OpenFoodApi =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
@@ -40,7 +43,15 @@ object TrackerDataModule {
             .build()
             .create()
 
-
+    @Provides
+    @Singleton
+    fun provideTrackerDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(
+            context,
+            TrackerDatabase::class.java,
+            "tracker_db"
+        )
+            .build()
 
 
 }
